41566d40655f687b74acc439c93a02c3b10eb0fe
Krizsan0596 <kristof.nagy.lazar@gmail.com>
2025-12-08 16:20:11 +0100

    refactor(file): use mmap for file reading, update API
    Switch file reading from malloc/fread to mmap for improved performance
    and memory management. Update read_raw to return const char* and require
    munmap for cleanup. Refactor all usages and tests to handle mmap'd data
    correctly. Fixes memory leaks and improves large file handling.

--- lib/compress.h
+++ lib/compress.h
@@ -4,15 +4,15 @@
 #include "data_types.h"
 #include <stdbool.h>
 
-int count_frequencies(char *data, long data_len, long *frequencies);
+int count_frequencies(const char *data, long data_len, long *frequencies);
 Node* construct_tree(Node *nodes, long leaf_count);
 Node construct_leaf(long frequency, char data);
 Node construct_branch(Node *nodes, int left_index, int right_index);
 void sort_nodes(Node *nodes, int len);
 char* check_cache(char leaf, char **cache);
 char* find_leaf(char leaf, Node *nodes, Node *root_node);
-int compress(char *original_data, long data_len, Node *nodes, Node *root_node, char** cache, Compressed_file *compressed_file);
+int compress(const char *original_data, long data_len, Node *nodes, Node *root_node, char** cache, Compressed_file *compressed_file);
 char* generate_output_file(char *input_file);
-int run_compression(Arguments args, char *data, long data_len, long directory_size);
+int run_compression(Arguments args, const char *data, long data_len, long directory_size);
 
 #endif
